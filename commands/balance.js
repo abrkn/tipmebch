643074d8919a1764fe13b8d00a01538d635c8f5b
Andreas Brekken <andreas@brekken.com>
2018-02-25 13:10:11 -0400

    Add balance stickers

--- commands/balance.js
+++ commands/balance.js
@@ -1,6 +1,15 @@
 const assert = require('assert');
 const { formatConfirmedAndUnconfirmedBalances } = require('../apis');
-const { getBalanceForUser } = require('../apis');
+const { getBalanceForUser, bchToUsd } = require('../apis');
+
+const { BALANCE_STICKERS } = process.env;
+
+const balanceStickers = BALANCE_STICKERS.split(/,/g)
+  .reduce((prev, pair) => {
+    const [level, stickerId] = pair.split(/:/g);
+    return [...prev, { level: +level, stickerId }];
+  }, [])
+  .reverse();
 
 module.exports = async ({ ctx, userId, fetchRpc }) => {
   const confirmed = await getBalanceForUser(userId, { fetchRpc });
