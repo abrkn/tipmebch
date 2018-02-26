a275a1c6f3d63b0b12711709468cdc5755260597
Andreas Brekken <andreas@brekken.com>
2018-02-26 13:05:28 -0400

    Remove tipmebch

--- commands/index.js
+++ commands/index.js
@@ -3,7 +3,6 @@ exports.balance = require('./balance');
 exports.balancereport = require('./balanceReport');
 exports.deposit = require('./deposit');
 exports.withdraw = require('./withdraw');
-exports.tipmebch = require('./tipmebch');
 exports.help = require('./help');
 exports.about = require('./about');
 exports.stats = require('./stats');
--- commands/tipmebch.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({ ctx, redisClient, reply, username }) => {
-  if (!ctx.userWasAlreadyKnown) {
-    return;
-  }
-
-  await reply(`I already know who you are, @${username}`);
-};
