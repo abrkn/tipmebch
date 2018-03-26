0188cc9fe309e9bf09e691752801657fefa92dff
Andreas Brekken <andreas@brekken.com>
2018-03-26 10:58:20 +0900

    Fix USD formatting in stats response.

--- commands/stats.js
+++ commands/stats.js
@@ -1,3 +1,5 @@
+const { formatUsd } = require('../utils');
+
 module.exports = async ({ reply, redisClient, ctx }) => {
   const [
     tippedBch,
@@ -9,10 +11,10 @@ module.exports = async ({ reply, redisClient, ctx }) => {
     introCount,
   ] = await Promise.all([
     redisClient.getAsync('stats.tipped.bch'),
-    redisClient.getAsync('stats.tipped.usd'),
+    formatUsd(redisClient.getAsync('stats.tipped.usd')),
     redisClient.getAsync('stats.tipped.count'),
     redisClient.getAsync('stats.withdrawn.bch'),
-    redisClient.getAsync('stats.withdrawn.usd'),
+    formatUsd(redisClient.getAsync('stats.withdrawn.usd')),
     redisClient.getAsync('stats.withdrawn.count'),
     redisClient.getAsync('stats.intros.count'),
   ]);
@@ -23,8 +25,8 @@ module.exports = async ({ reply, redisClient, ctx }) => {
     [
       `${introCount || 0} have introduced themselves to me`,
       `Users have tipped ${tippedCount || 0} times, totaling ${tippedBch ||
-        0} BCH ($${tippedUsd || 0})`,
-      `${withdrawnBch || 0} BCH ($${withdrawnUsd ||
+        0} BCH (${tippedUsd || 0})`,
+      `${withdrawnBch || 0} BCH (${withdrawnUsd ||
         0}) has been sent out in ${withdrawnCount || 0} withdraws`,
     ].join('\n')
   );
